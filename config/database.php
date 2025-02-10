4ba8285ee8fe03575a23977df9eb7ec7a21433f7
Z0UH4IRI <mohamedzouhairi023@gmail.com>
2025-02-10 16:02:16 +0100

    Adding Connection via PostgresSQL Elequent

--- config/database.php
+++ config/database.php
@@ -5,23 +5,29 @@ namespace App\Config;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 class Database {
-    public static function init() 
+    public static function init()
     {
-        $config = [
-            'driver'    => $_ENV['DB_CONNECTION'] ?? 'mysql',
-            'host'      => $_ENV['DB_HOST'] ?? '127.0.0.1',
-            'port'      => $_ENV['DB_PORT'] ?? '3306',
-            'database'  => $_ENV['DB_DATABASE'] ?? 'framework',
-            'username'  => $_ENV['DB_USERNAME'] ?? 'root',
-            'password'  => $_ENV['DB_PASSWORD'] ?? '',
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-        ];
-        
-        $capsule = new Capsule;
-        $capsule->addConnection($config);
-        $capsule->setAsGlobal();
-        $capsule->bootEloquent();
+        try {
+            $config = [
+                'driver'    => $_ENV['DB_CONNECTION'],
+                'host'      => $_ENV['DB_HOST'],
+                'port'      => $_ENV['DB_PORT'],
+                'database'  => $_ENV['DB_DATABASE'],
+                'username'  => $_ENV['DB_USERNAME'],
+                'password'  => $_ENV['DB_PASSWORD'],
+                'charset'   => 'utf8',
+                'collation' => 'utf8_unicode_ci',
+                'prefix'    => '',
+            ];
+
+            $capsule = new Capsule;
+            $capsule->addConnection($config);
+            $capsule->setAsGlobal();
+            $capsule->bootEloquent();
+
+            echo "✅ Connexion réussie à PostgreSQL via Eloquent!";
+        } catch (\Exception $e) {
+            die("❌ Erreur de connexion : " . $e->getMessage());
+        }
     }
 }
